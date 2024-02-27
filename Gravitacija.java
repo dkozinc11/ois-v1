c6e2a193a56f63866b70ef6b3cc68169fbf75e57
dkozinc11 <deniskozinc233@gmail.com>
2024-02-27 21:31:31 +0100

    Končna verzija aplikacije za gravitacijo

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,6 +1,12 @@
 public class Gravitacija {
     public static void main(String[] args) {
         System.out.println("OIS je zakon!");
+
+        izpis(0.0);
+        izpis(10.0);
+        izpis(1000.0);
+        izpis(100000.0);
+
     }
     public static void izpis(double visina) {
         System.out.printf("Gravitacijski pospešek na %.2f km nadmorske višine je %.2f m/s^2\n",
