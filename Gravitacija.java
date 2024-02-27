1bf30123a53c6219b36b9764ec3638a1530a88b5
dkozinc11 <deniskozinc233@gmail.com>
2024-02-27 21:04:13 +0100

    Pripravljena funkcija za izpis

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,6 +1,10 @@
 public class Gravitacija {
     public static void main(String[] args) {
-        System.out.println("OIS je zakon! Se strinjam.");
+        System.out.println("OIS je zakon!");
+    }
+    public static void izpis(double visina) {
+        System.out.printf("Gravitacijski pospešek na %.2f km nadmorske višine je %.2f m/s^2\n",
+         visina, izracunGravitacije(visina * 1000));;
     }
     public static double izracunGravitacije(double visina) {
         double C = 6.674e-11;
